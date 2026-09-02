2bc521166f22571ef1ad3f1ba024e6fa8a3c28b2
María Magdalena Bueno Pedrera <mmbp0003@red.ujaen.es>
2026-09-02 12:44:46 +0200

    Borrar género con libros asociados

--- src/main/java/es/ujaen/librosApp/LibroAppApplication.java
+++ src/main/java/es/ujaen/librosApp/LibroAppApplication.java
@@ -1,10 +1,8 @@
 package es.ujaen.librosApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class LibroAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(LibroAppApplication.class, args);
--- src/main/java/es/ujaen/librosApp/Service/GeneroService.java
+++ src/main/java/es/ujaen/librosApp/Service/GeneroService.java
@@ -1,9 +1,11 @@
 package es.ujaen.librosApp.Service;
 
 import es.ujaen.librosApp.model.Genero;
+import es.ujaen.librosApp.model.Libro;
 import es.ujaen.librosApp.repository.GeneroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,7 +25,15 @@ public class GeneroService {
     }
 
     //Solo para admn
+    @Transactional
     public void borrar(int id) {
+        Genero genero = generoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Género no encontrado"));
+
+        for (Libro libro : genero.getLibros()) {
+            libro.getGeneros().remove(genero);
+        }
+
         generoRepository.deleteById(id);
     }
 }
